f40a945b6cf2b573ff37f1986436a37458393e4e
Rodrigo Medeiros <rmedeiros@techsocial.com.br>
2019-08-02 23:12:09 -0300

    update eta component to react

--- src/components/Eta.js
+++ src/components/Eta.js
@@ -10,76 +10,48 @@ class Eta extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: 0,
-      lon: 0,
-      speed: 0,
-      dist: null,
-      etaUnitIndex: 0,
-      etaLabel: "",
-      etaUnit: ""
+      etaUnitIndex: 0
     };
   }
 
-  componentDidMount() {
-    this.updateLabels();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.position !== this.props.position) {
-      this.setPosition(this.props.position);
-    }
-  }
-
-  setPosition = position => {
-    if (!position || !position.coords || !position.coords.latitude) {
-      this.setState({ lat: null, lon: null });
-    } else {
-      this.setState({
-        lat: position.coords.latitude,
-        lon: position.coords.longitude,
-        speed: position.coords.speed
-      });
-    }
-    this.updateLabels();
-  };
-
-  updateLabels = () => {
-    const etaUnit = Config.timeUnits[this.state.etaUnitIndex];
-    let etaLabel = "N/A";
-    if (
-      this.state.lat &&
-      this.state.lon &&
-      this.state.speed &&
-      this.props.waypoint
-    ) {
-      const dist = getDistance(
-        {
-          latitude: this.state.lat,
-          longitude: this.state.lon
-        },
-        {
-          latitude: this.props.waypoint.lat,
-          longitude: this.props.waypoint.lon
-        }
-      );
-      this.setState({ dist });
-      const eta = dist / this.state.speed;
-
-      const calceta = eta * etaUnit.factor;
-      etaLabel = calceta.toFixed(calceta < 10 ? 1 : 0);
-    }
-    this.setState({ etaLabel, etaUnit: etaUnit.symbol });
-  };
-
   changeUnit = () => {
     const units = Config.timeUnits.length - 1;
-    if (this.state.etaUnitIndex < units) this.state.etaUnitIndex++;
-    else this.state.etaUnitIndex = 0;
-    this.updateLabels();
+    const etaUnitIndex =
+      this.state.etaUnitIndex < units ? this.state.etaUnitIndex + 1 : 0;
+    this.setState({ etaUnitIndex });
+  };
+
+  getUnitFactor = () => {
+    return Config.timeUnits[this.state.etaUnitIndex].factor;
+  };
+
+  getEtaUnit = () => {
+    return Config.timeUnits[this.state.etaUnitIndex].symbol;
+  };
+
+  getEtaLabel = () => {
+    const { coords } = this.props.position;
+    if (!coords || !coords.latitude || !coords.speed || !this.props.waypoint)
+      return "N/A";
+    const { latitude, longitude, speed } = coords;
+    const dist = getDistance(
+      {
+        latitude,
+        longitude
+      },
+      {
+        latitude: this.props.waypoint.lat,
+        longitude: this.props.waypoint.lon
+      }
+    );
+    const eta = dist / speed;
+    const calcEta = eta * this.getUnitFactor();
+    return calcEta.toFixed(calcEta < 10 ? 1 : 0);
   };
 
   render() {
-    const {} = this.state;
+    const etaLabel = this.getEtaLabel();
+    const etaUnit = this.getEtaUnit();
     return (
       <Touchable onPress={this.changeUnit} style={styles.button}>
         <View style={styles.container}>
@@ -102,7 +74,7 @@ class Eta extends Component {
               padding: 5
             }}
           >
-            <Text style={styles.number}>{this.state.etaLabel}</Text>
+            <Text style={styles.number}>{etaLabel}</Text>
           </View>
           <View
             style={{
@@ -113,7 +85,7 @@ class Eta extends Component {
               padding: 5
             }}
           >
-            <Text style={styles.text}>{this.state.etaUnit}</Text>
+            <Text style={styles.text}>{etaUnit}</Text>
           </View>
         </View>
       </Touchable>
